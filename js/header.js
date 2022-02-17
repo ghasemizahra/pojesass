ea0c5b160de2eeb05983d11226ec953becbdbe64
Zahra <z.ghasemi.02018@gmail.com>
2022-02-17 23:18:20 +0330

    update

--- js/header.js
+++ js/header.js
@@ -54,3 +54,38 @@ function openMenu() {
     );
   }
 });
+//این قسمت برای متن های اسلایدر
+$(document).ready(function(){
+//   $(".contentslider1").css("color", "red")
+//       .slideUp(2000)
+//       .slideDown(2000);
+ 
+    $(".contentslider1").hide();
+    $(".contentslider1").show();
+
+});
+
+$(document).ready(function(){
+
+    $(".contentslider2").animate({top: '-50px'});
+  });
+  //اینجا اولین منو
+
+  $(document).ready(function(){
+    $(".m1").hover(function(){
+      $(this).css({"z-index":"-1","left":"0","right": "100%","bottom":"0","background":"#6fb586", "height":"1px"});
+      }, function(){
+      $(this).css();
+    });})
+    // 
+    // 
+   
+    
+    // -webkit-transition-property: "right";
+    // transition-property: "right";
+    // -webkit-transition-duration: .3s;
+    // transition-duration: .3s;
+    // -webkit-transition-timing-function: ease-out;
+    // transition-timing-function: ease-out;
+
+
